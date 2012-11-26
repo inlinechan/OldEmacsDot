111249610f9f19b3e4001615ed48ebb04662506c
Hyungchan Kim <hyungchan2.kim@lge.com>
2012-11-26 10:57:17 +0900

    Add auto-complete mode
    - initial support of auto-complete-mode with clang
    - arrange Makefile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,6 +45,8 @@
 (defvar section-dot t)
 (defvar section-php t)
 
+(defvar section-autocomplete t)
+
 ;;** Environment
 
 (when section-environment (message "Environment...")
@@ -593,3 +595,42 @@ vi style of % jumping to matching brace."
       (require 'php-mode)
       (message "php... done"))
 
+(when section-autocomplete (message "autocomplete...")
+	  (add-to-list 'load-path "~/.emacs.d/auto-complete-1.3.1")
+      (setq ac-dictionary-directories "~/.emacs.d/dict")
+      (require 'auto-complete-config)
+      (ac-config-default)
+      (setq ac-auto-start nil)
+      (ac-set-trigger-key "TAB")
+
+      (require 'auto-complete-clang)
+      ;; c++
+      (add-hook 'c++-mode (lambda ()
+                            (add-to-list 'ac-sources 'ac-source-clang)))
+
+      ;; Complete member name by C-c . for C++ mode.
+      (add-hook 'c++-mode-hook
+                (lambda ()
+                  (local-set-key (kbd "C-c .") 'ac-complete-clang)))
+
+      (setq ac-clang-flags
+            (mapcar (lambda (item)(concat "-I" item))
+                    (split-string
+                     "
+ /usr/include/c++/4.6
+ /usr/include/c++/4.6/x86_64-linux-gnu/.
+ /usr/include/c++/4.6/backward
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include
+ /usr/local/include
+ /usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed
+ /usr/include/x86_64-linux-gnu
+ /usr/include
+"
+                     )))
+
+      (defun my-ac-cc-mode-setup ()
+        (setq ac-sources (append '(ac-source-clang ac-source-yasnippet) ac-sources)))
+      (add-hook 'c++-mode-hook 'my-ac-cc-mode-setup)
+      (add-hook 'c-mode-common-hook 'my-ac-cc-mode-setup)
+
+      (message "autocomplete... done"))
